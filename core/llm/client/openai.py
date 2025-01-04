8cd90ceb3c8fb7c56acca20ca1bc8639336a47d7
sunminghui <sunminghui@howzhi.com>
2025-01-04 23:05:14 +0800

    support deepseek

--- core/llm/client/openai.py
+++ core/llm/client/openai.py
@@ -13,10 +13,11 @@ class OpenAIClient(BaseClient):
         if not os.getenv("OPENAI_API_KEY"):
             load_dotenv()
         self.api_key = api_key or os.getenv("OPENAI_API_KEY")
+        self.base_url = os.getenv("OPENAI_API_BASE_URL", "https://api.openai.com")
         if not self.api_key:
             raise ValueError("API key is required. Please provide it or set it in the environment variables.")
 
-        self.client = OpenAI(api_key=api_key)
+        self.client = OpenAI(api_key=self.api_key, base_url=self.base_url)
         self.default_model = os.getenv("OPENAI_API_MODEL", "gpt-4o-mini")
 
     def completions(self,
