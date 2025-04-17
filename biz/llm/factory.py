85a462ef9032b85614b3d70b84992e32c562d2e1
曾铭新 <8574157@qq.com>
2025-04-17 08:35:07 +0800

    LLM供应商增加通义千问

--- biz/llm/factory.py
+++ biz/llm/factory.py
@@ -4,6 +4,7 @@ from biz.llm.client.base import BaseClient
 from biz.llm.client.deepseek import DeepSeekClient
 from biz.llm.client.ollama_client import OllamaClient
 from biz.llm.client.openai import OpenAIClient
+from biz.llm.client.qwen import QwenClient
 from biz.llm.client.zhipuai import ZhipuAIClient
 from biz.utils.log import logger
 
@@ -16,6 +17,7 @@ class Factory:
             'zhipuai': lambda: ZhipuAIClient(),
             'openai': lambda: OpenAIClient(),
             'deepseek': lambda: DeepSeekClient(),
+            'qwen': lambda: QwenClient(),
             'ollama': lambda : OllamaClient()
         }
 
